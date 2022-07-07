609ce356a0014c8dedd897d5ba3b8bc1262ed489
erijMbb <erijmaherziabenbrahim@gmail.com>
2022-07-07 12:43:23 +0100

    update candidat

--- src/components/Update.js
+++ src/components/Update.js
@@ -2,43 +2,39 @@ import React, { useContext } from 'react'
 import { useParams, useNavigate } from 'react-router-dom';
 import { candidatCtx } from './../store/CandidatContext';
 import { useRef } from 'react';
+import { useState } from 'react';
 
 export default function Update() {
-  let id=useParams()
+  let {id}=useParams()
   const candCtx= useContext(candidatCtx)
-  const navigate=useNavigate()
-  const refFname= useRef('')
-  const refLname= useRef('')
-  const refAge= useRef('')
-  const refProfession= useRef('')
-  const refAvatar= useRef('')
-  function submitHandler(e){
-   e.preventDefault()
-   let CandidatUpdate={
-     firstname: refFname.current.value,
-     lastname: refLname.current.value,
-     age: refAge.current.value,
-     profession: refProfession.current.value,
-     avatar: refAvatar.current.value,
-   }
-   candCtx.updateCandidat(CandidatUpdate)
-   navigate("/")
-  }
+let selectedCand= candCtx.getCandidatById(id)
+const [candidat, setCandidat]=useState(selectedCand)
+function handleInputChange ( event) {
+  const { name, value } = event.target
+
+  setCandidat({ ...candidat, [name]: value })
+}
+  let navigate=useNavigate()
   return (
     <div>
      <div>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={event => {
+        event.preventDefault()
+console.log('hi');
+        candCtx.updateCandidat(id,candidat)
+     navigate('/') }
+      }>
         <div className="container">
           <label htmlFor="firstname">Prénom</label>
-          <input className="form-control" type="text" ref={refFname} name="firstname"></input>
+          <input className="form-control" type="text" name="firstname" value={candidat.firstname} onChange={handleInputChange} ></input>
           <label htmlFor="lastname">Nom</label>
-          <input className="form-control" type="text" ref={refLname} name="lastname"></input>
+          <input className="form-control" type="text"  name="lastname" value={candidat.lastname} onChange={handleInputChange}></input>
           <label htmlFor="age">Age</label>
-          <input className="form-control" type="number" ref={refAge} name="age"></input>
+          <input className="form-control" type="number"  name="age" value={candidat.age} onChange={handleInputChange}></input>
           <label htmlFor="profession">Profession</label>
-          <input className="form-control" type="text" ref={refProfession} name="profession"></input>
+          <input className="form-control" type="text" name="profession"value={candidat.profession} onChange={handleInputChange}></input>
           <label htmlFor="avatar">Avatar</label>
-          <input className="form-control" type="url" ref={refAvatar} name="avatar"></input>
+          <input className="form-control" type="url" name="avatar"value={candidat.avatar}onChange={handleInputChange} ></input>
       <button className="btn btn-success my-2" type="submit">Modifier le Candidat</button>
         </div>
       </form>
--- src/store/CandidatContext.js
+++ src/store/CandidatContext.js
@@ -1,50 +1,47 @@
 import { createContext, useState } from "react";
 
-export const candidatCtx= createContext({
-    tabCandidats:[],
-    addNewCandidat:()=>{},
-    removeOneCandidat:()=>{},
-    getCandidatById:()=>{},
-    updateCandidat:()=>{}
-})
-function AddNewCandidatContextProvider(props){
-    const[tabCand,setTabCand]= useState([])
-    function addNewCandidat(newCandidat){
-        setTabCand((prev)=>{
-            return [...prev, newCandidat]
-        })
-   
-    }
-    function removeOneCandidat(id){
-        setTabCand(()=>{
-            return tabCand.filter((c)=>c.id !==id)
-        })
-    }
-    function getCandidatById(id){
-        
-         return   tabCand.find((c)=>c.id ===id)
-    
-    }
-    function updateCandidat(id){
-        let candidatToUpdate= tabCand.find((c)=>c.id ===id)
-            setTabCand((prev)=>{
-                prev.tabCand.map((c)=>{
-            return c.id===id? candidatToUpdate:c;
-                })
-                
-            })
-    }
-    const context={
-        tabCandidats:tabCand,
-        addNewCandidat:addNewCandidat,
-        removeOneCandidat:removeOneCandidat,
-        getCandidatById:getCandidatById,
-        updateCandidat:updateCandidat
-    }
-    return (
-        <candidatCtx.Provider value={context}>
-            {props.children}
-        </candidatCtx.Provider>
-    )
+export const candidatCtx = createContext({
+  tabCandidats: [],
+  addNewCandidat: () => {},
+  removeOneCandidat: () => {},
+  getCandidatById: () => {},
+  updateCandidat: () => {},
+});
+function AddNewCandidatContextProvider(props) {
+  const [tabCand, setTabCand] = useState([]);
+  function addNewCandidat(newCandidat) {
+    setTabCand((prev) => {
+      return [...prev, newCandidat];
+    });
+  }
+  function removeOneCandidat(id) {
+    setTabCand(() => {
+      return tabCand.filter((c) => c.id !== id);
+    });
+  }
+  function getCandidatById(id) {
+    return tabCand.find((c) => c.id === id);
+  }
+  function updateCandidat(id, updatedC) {
+    setTabCand(
+      tabCand.map((c) => ( (c.id === id? updatedC:c) 
+          )
+
+      )
+    );
+  }
+
+  const context = {
+    tabCandidats: tabCand,
+    addNewCandidat: addNewCandidat,
+    removeOneCandidat: removeOneCandidat,
+    getCandidatById: getCandidatById,
+    updateCandidat: updateCandidat,
+  };
+  return (
+    <candidatCtx.Provider value={context}>
+      {props.children}
+    </candidatCtx.Provider>
+  );
 }
 export default AddNewCandidatContextProvider;
